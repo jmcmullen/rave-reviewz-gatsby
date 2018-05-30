591e47e7abc00d49a13d1f52c990df2ed5bbca94
Jay McMullen <j.mcmullen@koala.com>
2018-05-30 19:46:01 +1000

    event proxy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ class Navbar extends Component {
     const eventsMobile = (
       <a
         className="menu-item"
-        href="https://www.ravereviewz.net/Events-Location/Sydney"
+        href="/Events-Location/Sydney"
         onClick={this.closeMenu}
         key="events"
       >
@@ -81,11 +81,7 @@ class Navbar extends Component {
     );
 
     const eventsDesktop = (
-      <a
-        className="navbar-item"
-        href="https://www.ravereviewz.net/Events-Location/Sydney"
-        key="events"
-      >
+      <a className="navbar-item" href="/Events-Location/Sydney" key="events">
         Events
       </a>
     );
